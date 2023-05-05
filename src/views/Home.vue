493bc9dfd70c7d239994aa724e0eb70336bc4e23
kaguithiajohnson@gmail.com <kaguithiajohnson@gmail.com>
2023-05-05 20:28:04 +0300

    Create the homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,28 @@
 <template>
-  Home
+<div class="flex flex-col p-8">
+  <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search for meals">
+
+  <div class="flex justify-center gap-2 mt-2">
+    <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters.split('')" :key="letter">
+      {{ letter }}
+    </router-link>
+
+  </div>
+</div>
+
 </template>
 
 <script setup>
+    import { computed, onMounted } from 'vue'
+    import store from '../store'
+    import axiosClient from '../axiosClient.js'
+
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    
+    onMounted( async () => {
+      const response = await axiosClient.get('/list.php?i=list')
+      console.log(response.data)
+    })
 
 </script>
 
