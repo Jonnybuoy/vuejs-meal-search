e2bb5f5cddeaea6fc5caf4ddf9643c133a3e2796
kaguithiajohnson@gmail.com <kaguithiajohnson@gmail.com>
2023-05-25 13:37:29 +0300

    Fix bug while searching for meals by ingredient.

--- src/views/MealsByIngredients.vue
+++ src/views/MealsByIngredients.vue
@@ -1,20 +1,18 @@
 <template>
 
-<Meals :meals="meals"/>
+<Meals :meals="meals" />
 
 </template>
 
 <script setup>
 import { onMounted, ref, computed } from 'vue'
-import axiosClient from '../axiosClient'
 import store from '../store'
 import { useRoute } from 'vue-router'
 import Meals from '../components/Meals.vue'
 
 const route = useRoute();
-const meals = computed(() => {
-  store.state.mealsByIngredient
-})
+debugger;
+const meals = computed(() => store.state.mealsByIngredient)
 
 onMounted(() => {
   store.dispatch('searchMealsByIngredient', route.params.ingredient)
